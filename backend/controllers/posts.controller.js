7b7d1c78402beb354e8a9d518ed4c3a23e4cb70d
PasinduManeka <manekaherat815@gmail.com>
2021-08-05 02:04:37 +0530

    set the get function

--- backend/controllers/posts.controller.js
+++ backend/controllers/posts.controller.js
@@ -1,5 +1,21 @@
-const getPosts = (req,res)=>{
-    res.send('This Works!')
+const PostMessage = require("../models/postMessage.models.js")
+
+const getPosts = async(req,res)=>{
+    try{
+        const postMessages = await PostMessage.find();
+        console.log(postMessages);
+        res.status(200).json(postMessages);
+    }catch (error){
+        res.status(404).json({messsage:error.message});
+    }
 }
 
-module.exports={getPosts};
+const createPost = (req,res)=>{
+    try{
+
+    }catch (error){
+
+    }
+}
+
+module.exports={getPosts, createPost};
--- backend/routes/posts.routes.js
+++ backend/routes/posts.routes.js
@@ -1,11 +1,12 @@
 const express =require("express");
 
 //import control class methods.
-const {getPosts} = require("../controllers/posts.controller.js");
+const {getPosts, createPost} = require("../controllers/posts.controller.js");
 
 const router = express.Router();
 
 //Diaplay the records in database (GET)
 router.get("/",getPosts);
+router.get("/addPOst",createPost)
 
 module.exports=router;
