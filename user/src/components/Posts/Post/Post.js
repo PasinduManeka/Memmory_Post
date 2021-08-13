2b5ad6f05a36165452cdad0c1fee051f0872fbbc
PasinduManeka <manekaherat815@gmail.com>
2021-08-13 19:17:49 +0530

    added a ui package

--- user/src/components/Posts/Post/Post.js
+++ user/src/components/Posts/Post/Post.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
+//import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+//import DeleteIcon from '@material-ui/icons/Delete';
+//import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 
 import useStyles from './styles';
 
-const Post = () => {
+
+const Post = ({ post }) => {
     const classes = useStyles();
     return ( 
-        <div className={classes.title}><h1>Post</h1></div>
-        
+        <Card className={classes.card}>
+            <CardMedia className={classes.media} image={post.selectedFile} title={post.title} />
+        </Card>
      );
 }
  
