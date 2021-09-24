e20a474cc5154b177e94a8e4dcc5cf6f961f7f73
PasinduManeka <manekaherat815@gmail.com>
2021-09-25 02:36:44 +0530

    CREATE: created a new navbar.

--- user/src/components/Navbar/Navbar.js
+++ user/src/components/Navbar/Navbar.js
@@ -1,17 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { AppBar, Typography } from "@material-ui/core";
+
+import useStyles from "./stytles";
+import memories from "../../images/memories-word-metal-type-mixed-vintage-printing-blocks-over-grunge-wood-50272791.jpg";
 
 const Navbar = () => {
+  const classes = useStyles();
   return (
     <AppBar className={classes.appBar} position="static" color="inherit">
-      <Typography className={classes.heading} variant="h2" align="center">
-        Memmories
-      </Typography>
-      <img
-        className={classes.image}
-        src={memories}
-        alt="memories"
-        height="60"
-      />
+      <div className={classes.brandContainer}>
+        <Typography
+          component={Link}
+          to="/"
+          className={classes.heading}
+          variant="h2"
+          align="center"
+        >
+          Memmories
+        </Typography>
+        <img
+          className={classes.image}
+          src={memories}
+          alt="memories"
+          height="60"
+        />
+      </div>
     </AppBar>
   );
 };
