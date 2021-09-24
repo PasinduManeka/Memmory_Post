7bfb1c5c35b851e198a706809137608cdebd3fe4
PasinduManeka <manekaherat815@gmail.com>
2021-09-24 07:50:52 +0530

    CREATE: created the like count function in frontend.

--- user/src/api/index.js
+++ user/src/api/index.js
@@ -15,3 +15,6 @@ export const updatePost = (id, updatedPost) =>
 
 //* DELETE
 export const deletePost = (id) => axios.delete(`${url}/${id}`);
+
+//*like account
+export const likePost = (id) => axios.patch(`${url}/${id}/likePost`);
